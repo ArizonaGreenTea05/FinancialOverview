ab68bf136a213ac9ecb89cac7d2a5787aa368c90
Tobias <84347307+ArizonaGreenTea05@users.noreply.github.com>
2023-06-29 19:37:57 +0200

    Cleanup (#3)
    * remove unused class
    
    * cleanup

--- MauiMoneyMate/CommonVariables.cs
+++ MauiMoneyMate/CommonVariables.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Graphics.Text;
-
-namespace MauiMoneyMate
+﻿namespace MauiMoneyMate
 {
     public class CommonVariables
     {
--- MauiMoneyMate/ViewModels/FileViewModel.cs
+++ MauiMoneyMate/ViewModels/FileViewModel.cs
@@ -1,128 +1,144 @@
-﻿using System.Text;
-using BusinessLogic;
+﻿using BusinessLogic;
 using CommunityToolkit.Maui.Alerts;
-using CommunityToolkit.Mvvm.ComponentModel;
-using MauiMoneyMate.Resources.Languages;
-using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Maui.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiMoneyMate.Resources.Languages;
 
-namespace MauiMoneyMate.ViewModels
+namespace MauiMoneyMate.ViewModels;
+
+public partial class FileViewModel : ObservableObject
 {
-    public partial class FileViewModel : ObservableObject
+    #region private Observable Properties
+
+    [ObservableProperty] private string _filePageTitle;
+
+    [ObservableProperty] private string _openFileText;
+
+    [ObservableProperty] private string _saveFileText;
+
+    [ObservableProperty] private string _saveFileAsText;
+
+    [ObservableProperty] private int _openFileFontSize;
+
+    [ObservableProperty] private int _saveFileFontSize;
+
+    [ObservableProperty] private int _saveFileAsFontSize;
+
+    #endregion
+
+    #region private Members
+
+    private readonly FinancialOverview _financialOverview;
+    private readonly CommonVariables _commonVariables;
+
+    #endregion
+
+    #region public Constructors
+
+    public FileViewModel(FinancialOverview financialOverview, CommonVariables commonVariables)
     {
+        _commonVariables = commonVariables;
+        _financialOverview = financialOverview;
+        LoadResources();
+    }
 
-        public enum FileDialogType
+    #endregion
+
+    #region private Relay Commands
+
+    [RelayCommand]
+    private async Task OpenFile(CancellationToken cancellationToken)
+    {
+        try
         {
-            Save,
-            Open
-        }
-
-        [ObservableProperty] private string _filePageTitle;
-
-        [ObservableProperty] private string _openFileText;
-
-        [ObservableProperty] private string _saveFileText;
-
-        [ObservableProperty] private string _saveFileAsText;
-
-        [ObservableProperty] private int _openFileFontSize;
-
-        [ObservableProperty] private int _saveFileFontSize;
-
-        [ObservableProperty] private int _saveFileAsFontSize;
-
-        private readonly FinancialOverview _financialOverview;
-        private readonly CommonVariables _commonVariables;
-
-        public FileViewModel(FinancialOverview financialOverview, CommonVariables commonVariables)
-        {
-            _commonVariables = commonVariables;
-            _financialOverview = financialOverview;
-            LoadResources();
-        }
-
-        [RelayCommand]
-        private async Task OpenFile(CancellationToken cancellationToken)
-        {
-            try
+            var folderPickerResult = await FilePicker.PickAsync(PickOptions.Default);
+            if (folderPickerResult == null)
             {
-                var folderPickerResult = await FilePicker.PickAsync(PickOptions.Default);
-                if (folderPickerResult == null)
-                {
-                    Toast.Make("File could not be opened");
-                    return;
-                }
-
-                _financialOverview.LoadData(folderPickerResult.FullPath);
-                DataIsSaved = true;
-            }
-            catch (Exception ex)
-            {
-                await Toast.Make($"File could not be opened, {ex.Message}").Show(cancellationToken);
+                Toast.Make("File could not be opened");
+                return;
             }
 
+            _financialOverview.LoadData(folderPickerResult.FullPath);
+            DataIsSaved = true;
         }
-        [RelayCommand]
-        private async Task SaveFile(CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            DataIsSaved = _financialOverview.SaveData();
-            if (DataIsSaved)
-                await Toast.Make($"File has been saved: {_financialOverview.DefaultFilePath}").Show(cancellationToken);
-            else
-                await Toast.Make($"File could not be saved").Show(cancellationToken);
-        }
-
-        [RelayCommand]
-        private async Task SaveFileAs(CancellationToken cancellationToken)
-        {
-            try
-            {
-                var fileLocationResult = await FileSaver.SaveAsync(_financialOverview.DefaultDirectory, _financialOverview.DefaultFilename, Stream.Null, cancellationToken);
-                fileLocationResult.EnsureSuccess();
-                _financialOverview.SaveData(fileLocationResult.FilePath);
-                await Toast.Make($"File has been saved: {fileLocationResult.FilePath}").Show(cancellationToken);
-                DataIsSaved = true;
-            }
-            catch (Exception ex)
-            {
-                await Toast.Make($"File could not be saved, {ex.Message}").Show(cancellationToken);
-            }
-        }
-
-        public void OnAppearing()
-        {
-            DisplaySavingState();
-        }
-
-        private bool DataIsSaved
-        {
-            set
-            {
-                _commonVariables.DataIsSaved = value;
-                DisplaySavingState();
-            }
-            get => _commonVariables.DataIsSaved;
-        }
-
-        private void DisplaySavingState()
-        {
-            FilePageTitle = FilePageTitle.TrimEnd('*');
-            if (!DataIsSaved)
-                FilePageTitle += '*';
-        }
-
-        private void LoadResources()
-        {
-            FilePageTitle = TextResource.FilePageTitle ?? string.Empty;
-            OpenFileText = TextResource.OpenFile ?? string.Empty;
-            SaveFileText = TextResource.SaveFile ?? string.Empty;
-            SaveFileAsText = TextResource.SaveFileAs ?? string.Empty;
-            OpenFileFontSize = Convert.ToInt32(TextResource.OpenFileFontSize);
-            OpenFileFontSize = 0 == OpenFileFontSize ? 17 : OpenFileFontSize;
-            SaveFileFontSize = Convert.ToInt32(TextResource.SaveFileFontSize);
-            SaveFileFontSize = 0 == SaveFileFontSize ? 17 : SaveFileFontSize;
-            SaveFileAsFontSize = Convert.ToInt32(TextResource.SaveFileAsFontSize);
-            SaveFileAsFontSize = 0 == SaveFileAsFontSize ? 17 : SaveFileAsFontSize;
+            await Toast.Make($"File could not be opened, {ex.Message}").Show(cancellationToken);
         }
     }
+
+    [RelayCommand]
+    private async Task SaveFile(CancellationToken cancellationToken)
+    {
+        DataIsSaved = _financialOverview.SaveData();
+        if (DataIsSaved)
+            await Toast.Make($"File has been saved: {_financialOverview.DefaultFilePath}").Show(cancellationToken);
+        else
+            await Toast.Make("File could not be saved").Show(cancellationToken);
+    }
+
+    [RelayCommand]
+    private async Task SaveFileAs(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var fileLocationResult = await FileSaver.SaveAsync(_financialOverview.DefaultDirectory,
+                _financialOverview.DefaultFilename, Stream.Null, cancellationToken);
+            fileLocationResult.EnsureSuccess();
+            _financialOverview.SaveData(fileLocationResult.FilePath);
+            await Toast.Make($"File has been saved: {fileLocationResult.FilePath}").Show(cancellationToken);
+            DataIsSaved = true;
+        }
+        catch (Exception ex)
+        {
+            await Toast.Make($"File could not be saved, {ex.Message}").Show(cancellationToken);
+        }
+    }
+
+    #endregion
+
+    #region public Methods
+
+    public void OnAppearing()
+    {
+        DisplaySavingState();
+    }
+
+    #endregion
+
+    #region private Methods
+
+    private bool DataIsSaved
+    {
+        set
+        {
+            _commonVariables.DataIsSaved = value;
+            DisplaySavingState();
+        }
+        get => _commonVariables.DataIsSaved;
+    }
+
+    private void DisplaySavingState()
+    {
+        FilePageTitle = FilePageTitle.TrimEnd('*');
+        if (!DataIsSaved)
+            FilePageTitle += '*';
+    }
+
+    private void LoadResources()
+    {
+        FilePageTitle = TextResource.FilePageTitle ?? string.Empty;
+        OpenFileText = TextResource.OpenFile ?? string.Empty;
+        SaveFileText = TextResource.SaveFile ?? string.Empty;
+        SaveFileAsText = TextResource.SaveFileAs ?? string.Empty;
+        OpenFileFontSize = Convert.ToInt32(TextResource.OpenFileFontSize);
+        OpenFileFontSize = 0 == OpenFileFontSize ? 17 : OpenFileFontSize;
+        SaveFileFontSize = Convert.ToInt32(TextResource.SaveFileFontSize);
+        SaveFileFontSize = 0 == SaveFileFontSize ? 17 : SaveFileFontSize;
+        SaveFileAsFontSize = Convert.ToInt32(TextResource.SaveFileAsFontSize);
+        SaveFileAsFontSize = 0 == SaveFileAsFontSize ? 17 : SaveFileAsFontSize;
+    }
+
+    #endregion
 }
