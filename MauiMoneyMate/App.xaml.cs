02694f93893b14545fe1d9863ea7d5c99a560d3f
Tobias <84347307+ArizonaGreenTea05@users.noreply.github.com>
2023-08-06 11:25:13 +0200

    access the Window property as it was meant to (#73)

--- MauiMoneyMate/App.xaml.cs
+++ MauiMoneyMate/App.xaml.cs
@@ -4,20 +4,17 @@ namespace MauiMoneyMate;
 
 public partial class App : Application
 {
-    public static Window Window;
-
     public App()
     {
         InitializeComponent();
-
         MainPage = new AppShell();
     }
 
     protected override Window CreateWindow(IActivationState activationState)
     {
-        Window = base.CreateWindow(activationState);
-        Window.Title = nameof(MauiMoneyMate);
+        var window = base.CreateWindow(activationState);
+        window.Title = nameof(MauiMoneyMate);
         CommonFunctions.RemoveNonZipFiles(CommonProperties.UpdateDirectory);
-        return Window;
+        return window;
     }
 }
