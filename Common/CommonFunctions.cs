010e41d36aef78c57466ce0b031dd0f0941fe703
Tobias <tobias.schaelte@outlook.de>
2023-08-16 16:06:11 +0200

    Fix possible error on update checking

---
+++ Common/CommonFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace CommonLibrary
+{
+    public static class Functions
+    {
+        public static string ToHexString(string str)
+        {
+            var sb = new StringBuilder();
+
+            var bytes = Encoding.Unicode.GetBytes(str);
+            foreach (var t in bytes)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string FromHexString(string hexString)
+        {
+            var bytes = new byte[hexString.Length / 2];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return Encoding.Unicode.GetString(bytes);
+        }
+    }
+}
